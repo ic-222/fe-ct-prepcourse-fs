1647993128fe86d683b5d390a635eb870dbcc72e
ivy_ck <ivanna.so.cervantes@gmail.com>
2023-06-16 19:28:46 -0500

    hecho en clase

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -146,7 +146,7 @@ function operadoresLogicos(num1, num2, num3) {
    } else if (num1<0|| num2<0|| num3<0){
       return "Hay negativos"
    } else if (num3>num1&& num2){
-      return num3+1
+      return num3+1 // num3++
    } else if (num1==0 && num2==0 && num3==0){
       return "Error"
    } else {
--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -22,7 +22,11 @@ function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
-}
+   for (var i =0; i<array.length; i++) {
+      array[i]=array[i]+1;
+   }
+   return array
+} 
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
